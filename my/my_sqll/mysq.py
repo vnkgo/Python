fc72520591944595bd2dbd662a6e33265d4e1ac5
vnkfor <1272499410@qq.com>
2024-06-12 16:47:22 +0800

    tijiao

--- my/my_sqll/mysq.py
+++ my/my_sqll/mysq.py
@@ -1,7 +1,7 @@
 from pymysql import connect
 conn=connect(host='localhost',port=3306,user='root',password='root')
 cursor=conn.cursor()
-conn.select_db('myworld')d
+conn.select_db('myworld')
 cursor.execute("select * from student")
 
 
